53f50dcc8abc4d63812ebbbd8abd3aa84f615c4a
DDovhalenko <dimiyrg20001@gmail.com>
2022-10-30 11:41:39 +0100

    User authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  root 'home#homepage'
+  get 'homepage', to: 'home#homepage'
+  get 'mainpage', to: 'main#mainpage'
 end
