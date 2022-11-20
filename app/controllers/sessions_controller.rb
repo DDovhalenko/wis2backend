76fb9e5cde0d14b3c0251c7e31de9ffa7c128e4b
DDovhalenko <dimiyrg20001@gmail.com>
2022-11-20 20:18:39 +0100

    13

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,9 @@ class SessionsController < Devise::SessionsController
   
   def create
     @user = User.find_by_email(params[:user][:email])
-    #current_user = @user
     if(@user.valid_password?(params[:user][:password]))
       sign_in(:user, @user)
-      token = JsonWebToken.encode(user_id: @user.id)
+      #token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
       render json:{token: token, exp: time.strftime("%m-%d-%Y %H:%M"), user: @user}
       
