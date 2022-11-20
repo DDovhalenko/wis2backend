7ec264049b71873981a10b17529b45aeda7f873f
DDovhalenko <dimiyrg20001@gmail.com>
2022-11-20 20:05:06 +0100

    11

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,7 +11,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     allow do
         origins "https://wis2.vercel.app"
-        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        resource '*',
+        headers: ["Authorization"],
+        expose: ["Authorization"],
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        max_age: 600,
         credentials: true
     end
 end
