b75f2758ffe0e2567921eef4c878abe39a6a287e
DDovhalenko <dimiyrg20001@gmail.com>
2022-11-20 19:06:04 +0100

    10

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_wis2back", domain:"wis2back.herokuapp.com"
+    Rails.application.config.session_store :cookie_store, key: "_wis2back", domain:"wis2backend.vercel.app"
 else 
     Rails.application.config.session_store :cookie_store, key: "_wis2_app"
 end
