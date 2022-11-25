23c6f6ee0c40c5a11da7f907659a8f0b89025de6
DDovhalenko <dimiyrg20001@gmail.com>
2022-11-25 15:52:18 +0100

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       end
 
     allow do
-        origins "https://wis2.herokuapp.com"
+        origins "https://wis2front.herokuapp.com"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
     end
